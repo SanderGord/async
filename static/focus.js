ad1b76bd70177c5bff862eef76224b63c262529a
den-ibr <ibr.den.dan@gmail.com>
2025-04-29 17:33:52 +0500

    aaaaaaa

--- static/focus.js
+++ static/focus.js
@@ -1,41 +1,44 @@
 const API = {
-    organizationList: "/orgsList",
-    analytics: "/api3/analytics",
-    orgReqs: "/api3/reqBase",
-    buhForms: "/api3/buh",
+    organizationList: '/orgsList',
+    analytics: '/api3/analytics',
+    orgReqs: '/api3/reqBase',
+    buhForms: '/api3/buh',
 };
 
-function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
-        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
-            sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
-                addInOrgsMap(orgsMap, analytics, "analytics");
-                sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
-                    addInOrgsMap(orgsMap, buh, "buhForms");
-                    render(orgsMap, orgOgrns);
-                });
-            });
-        });
-    });
+async function run() {
+    const orgOgrns = await sendRequest(API.organizationList);
+    const ogrns = orgOgrns.join(',');
+
+    const requisites = await sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
+    const orgsMap = reqsToMap(requisites);
+    
+    const analytics = await sendRequest(`${API.analytics}?ogrn=${ogrns}`);
+    addInOrgsMap(orgsMap, analytics, 'analytics');
+
+    const buh = await sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
+    addInOrgsMap(orgsMap, buh, 'buhForms');
+    render(orgsMap, orgOgrns);
 }
 
-run();
+run().finally(() => {});
 
-function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
+function sendRequest(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.response));
+                } else {
+                    reject(new Error('плохо'));
+                }
             }
-        }
-    };
+        };
 
-    xhr.send();
+        xhr.send();
+    });
 }
 
 function reqsToMap(requisites) {
@@ -52,11 +55,11 @@ function addInOrgsMap(orgsMap, additionalInfo, key) {
 }
 
 function render(organizationsInfo, organizationsOrder) {
-    const table = document.getElementById("organizations");
-    table.classList.remove("hide");
+    const table = document.getElementById('organizations');
+    table.classList.remove('hide');
 
-    const template = document.getElementById("orgTemplate");
-    const container = table.querySelector("tbody");
+    const template = document.getElementById('orgTemplate');
+    const container = table.querySelector('tbody');
 
     organizationsOrder.forEach((item) => {
         renderOrganization(organizationsInfo[item], template, container);
@@ -65,14 +68,14 @@ function render(organizationsInfo, organizationsOrder) {
 
 function renderOrganization(orgInfo, template, container) {
     const clone = document.importNode(template.content, true);
-    const name = clone.querySelector(".name");
-    const indebtedness = clone.querySelector(".indebtedness");
-    const money = clone.querySelector(".money");
-    const address = clone.querySelector(".address");
+    const name = clone.querySelector('.name');
+    const indebtedness = clone.querySelector('.indebtedness');
+    const money = clone.querySelector('.money');
+    const address = clone.querySelector('.address');
 
     name.textContent =
         (orgInfo.UL && orgInfo.UL.legalName && orgInfo.UL.legalName.short) ||
-        "";
+        '';
     indebtedness.textContent = formatMoney(orgInfo.analytics.s1002 || 0);
 
     if (
@@ -89,7 +92,7 @@ function renderOrganization(orgInfo, template, container) {
                 0
         );
     } else {
-        money.textContent = "—";
+        money.textContent = '—';
     }
 
     const addressFromServer = orgInfo.UL.legalAddress.parsedAddressRF;
@@ -100,7 +103,7 @@ function renderOrganization(orgInfo, template, container) {
 
 function formatMoney(money) {
     let formatted = money.toFixed(2);
-    formatted = formatted.replace(".", ",");
+    formatted = formatted.replace('.', ',');
 
     const rounded = money.toFixed(0);
     const numLen = rounded.length;
@@ -114,19 +117,19 @@ function formatMoney(money) {
 function createAddress(address) {
     const addressToRender = [];
     if (address.regionName) {
-        addressToRender.push(createAddressItem("regionName"));
+        addressToRender.push(createAddressItem('regionName'));
     }
     if (address.city) {
-        addressToRender.push(createAddressItem("city"));
+        addressToRender.push(createAddressItem('city'));
     }
     if (address.street) {
-        addressToRender.push(createAddressItem("street"));
+        addressToRender.push(createAddressItem('street'));
     }
     if (address.house) {
-        addressToRender.push(createAddressItem("house"));
+        addressToRender.push(createAddressItem('house'));
     }
 
-    return addressToRender.join(", ");
+    return addressToRender.join(', ');
 
     function createAddressItem(key) {
         return `${address[key].topoShortName}. ${address[key].topoValue}`;
